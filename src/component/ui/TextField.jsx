0c9c1fdadd06fd772425718f27f94ff7a4c04c2f
snow <rhasya@gmail.com>
2024-03-29 12:25:12 +0900

    [CHANGE] change TextField component

--- src/component/ui/TextField.jsx
+++ src/component/ui/TextField.jsx
@@ -1,9 +1,10 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect, useImperativeHandle, useMemo, useRef, useState } from 'react';
 
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 
 import styles from './TextField.module.css';
+import del from '/ic_delete.svg';
 import hov from '/ic_send_hov.svg';
 import nor from '/ic_send_nor.svg';
 
@@ -18,17 +19,57 @@ const TextField = forwardRef(
    * } & React.ComponentPropsWithRef<'input'>} param0
    * @returns
    */
-  ({ value, hideBorder, onSend, ...rest }, ref) => {
+  ({ value, hideBorder, onSend, onChange, ...rest }, ref) => {
+    const inputRef = useRef(null);
+
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          focus() {
+            inputRef.current.focus();
+          },
+        };
+      },
+      [],
+    );
+
+    const isClearVisible = useMemo(() => value.length, [value]);
+    const [innerValue, setInnerValue] = useState('');
+    useEffect(() => {
+      setInnerValue(value);
+    }, [value]);
+
+    function handleChange(e) {
+      setInnerValue(e.target.value);
+      onChange?.(e);
+    }
+
+    function handleClearClick() {
+      setInnerValue('');
+      onChange?.({ target: inputRef.current });
+      inputRef.current.focus();
+    }
+
     return (
       <div className={styles.textfield}>
-        <input
-          type="search"
-          {...rest}
-          className={cn(styles.textfield__input, { [styles['textfield__input--border']]: !hideBorder })}
-          value={value}
-          ref={ref}
-        />
-        <input type="image" className={styles.textfield__send} src={!value ? nor : hov} onClick={onSend} />
+        <div className={styles.textfield__wrap}>
+          <input
+            type="text"
+            {...rest}
+            className={cn(styles.textfield__input, { [styles['textfield__input--border']]: !hideBorder })}
+            value={innerValue}
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <input
+            type="image"
+            src={del}
+            className={cn(styles.textfield__clear, { [styles['textfield__clear--show']]: isClearVisible })}
+            onClick={handleClearClick}
+          />
+        </div>
+        <input type="image" className={styles.textfield__send} src={!innerValue ? nor : hov} onClick={onSend} />
       </div>
     );
   },
@@ -40,6 +81,7 @@ TextField.propTypes = {
   value: PropTypes.string,
   hideBorder: PropTypes.bool,
   onSend: PropTypes.func,
+  onChange: PropTypes.func,
 };
 
 export default TextField;
